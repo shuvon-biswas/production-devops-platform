d8f8561c517ded5f3f14de6dfca3bd08b490b8ed
Shuvon Kumar Biswas <shuvon88@gmail.com>
2026-07-14 11:43:27 +0600

    Add public subnet resource

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,15 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
